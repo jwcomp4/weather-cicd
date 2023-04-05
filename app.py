ec79e6351cb9913fe2d3318511a5312443625130
John Compton <johncompton@Johns-MacBook-Air.local>
2023-04-05 10:12:58 -0500

    Expose app and server and remove unnecessary imports.

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import dash
 from dash import Dash, dcc, html, Input, Output, State
 import pandas as pd
 import os
-from datetime import datetime
 import requests
 import json
 import plotly
 import plotly.express as px
-import geopy
-from geopy.geocoders import Nominatim
 import plotly.graph_objects as go
+
+
+app = dash.Dash(__name__)
+server = app.server
