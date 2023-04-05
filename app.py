6a3a194bad091d23aa5812c227b1a4530c308e37
John Compton <johncompton@Johns-MacBook-Air.local>
2023-04-05 12:18:02 -0500

    Add start_date and end_date variables to app.py.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
-
+from utils.api_utils import start_date, end_date
 
 app = dash.Dash(__name__)
 server = app.server
