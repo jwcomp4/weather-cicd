608d1f72269c4f0277b33987ee46917fce7228b7
John Compton <johncompton@Johns-MacBook-Air.local>
2023-05-19 10:38:24 -0500

    Rearranged utility functions for cleaner code and imports.

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import dash
-from dash import Dash, dcc, html, Input, Output, State
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output, State, ctx
 import os
 import requests
-import json
-import plotly
-import plotly.express as px
 import dash_design_kit as ddk
-from utils.api_utils import start_end, locator, astro
-from utils.cleaning_utils import weather_clean, cw_filter, high_low
+from utils.cleaning_utils import get_weather
 from utils.controls import controls
-from utils.weather_cards import weather_card
-from config import key
+
 
 app = dash.Dash(__name__)
 server = app.server
 
+
 # Creating the Layout
 app.layout = ddk.App(
     [
+        dcc.Geolocation(id="geolocation"),
         ddk.Header(
             [
                 ddk.Logo(src=app.get_asset_url("/assets/logo.svg")),
@@ -59,39 +55,15 @@ def add_second_address(value):
 @app.callback(
     Output("weather1_row", "children"),
     Input("weather_button", "n_clicks"),
+    State("geolocation", "position"),
     State("address_value1", "value"),
     State("historical_year", "value"),
     prevent_initial_call=True,
 )
-def weather_request(n_clicks, address, year):
+def weather_request(n_clicks, pos, address, year):
     # This callback makes API calls and creates our visualizations.
-
-    # Geocoding the address and creating the locator object
-    location = locator.geocode(address)
-    # Grabbing the latitude and longitude
-    lat = location.latitude
-    lon = location.longitude
-    # Making the api request for the current weather.
-    current_weather = requests.get(
-        f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={lat},{lon}&days=7&aqi=no&alerts=no"
-    ).json()
-    # Getting today's date to be used in the ids of the ddk.DataCards
-    today = current_weather["location"]["localtime"][:10]
-    # Using the astro func defined in api_utils.py to get the sunrise, sunset, and moon phase
-    sunrise = astro(current_weather, "sunrise")
-    sunset = astro(current_weather, "sunset")
-    moon_phase = astro(current_weather, "moon_phase")
-    high_temp, low_temp = high_low(current_weather)
-
-    precip = cw_filter(current_weather, "Rain Chance", "chance_of_rain")
-    rain_today = px.bar(precip, x="Hour", y="Rain Chance")
-
-    # Establishing the start and end date for the historical weather api request
-    start_date, end_date = start_end(year)
-
-    weather_today = weather_card(
-        today, sunrise, sunset, moon_phase, high_temp, low_temp, rain_today
-    )
+    # This weather is for the first address and outputs to the first row
+    weather_today = get_weather(address, year)
     return weather_today
 
 
@@ -104,34 +76,12 @@ def weather_request(n_clicks, address, year):
 )
 def weather2_request(n_clicks, address, year):
     # This callback makes API calls and creates our visualizations.
-
-    # Geocoding the address and creating the locator object
-    location = locator.geocode(address)
-    # Grabbing the latitude and longitude
-    lat = location.latitude
-    lon = location.longitude
-    # Making the api request for the current weather.
-    current_weather = requests.get(
-        f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={lat},{lon}&days=7&aqi=no&alerts=no"
-    ).json()
-    # Getting today's date to be used in the ids of the ddk.DataCards
-    today = current_weather["location"]["localtime"][:10]
-    # Using the astro func defined in api_utils.py to get the sunrise, sunset, and moon phase
-    sunrise = astro(current_weather, "sunrise")
-    sunset = astro(current_weather, "sunset")
-    moon_phase = astro(current_weather, "moon_phase")
-    high_temp, low_temp = high_low(current_weather)
-
-    precip = cw_filter(current_weather, "Rain Chance", "chance_of_rain")
-    rain_today = px.bar(precip, x="Hour", y="Rain Chance")
-
-    # Establishing the start and end date for the historical weather api request
-    start_date, end_date = start_end(year)
-
-    weather_today = weather_card(
-        today, sunrise, sunset, moon_phase, high_temp, low_temp, rain_today
-    )
-    return weather_today
+    # This weather is for the second address (if given) and outputs to the second row
+    if address == None:
+        return dash.no_update
+    else:
+        second_weather = get_weather(address, year)
+        return second_weather
 
 
 if __name__ == "__main__":
--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -1,16 +1,46 @@
 import pandas as pd
+import dash_design_kit as ddk
+import requests
+from datetime import datetime
+import geopy
+from geopy.geocoders import GoogleV3
+from config import GOOGLE_MAP_KEY, key
+import json
+import plotly
+import plotly.express as px
 
-# Functions and other utilities for cleaning data.
+# Functions and other utilities for making API requests, filtering data, cleaning it, and vizualizing.
 
 
-# This function cleans data pulled from API requests for better graphing
-# Run this function after converting the API response to a pandas dataframe
-# The function takes the dataframe and the character on which a column needs to be split
+# Creating the locator object for geocoding:
+locator = GoogleV3(api_key=GOOGLE_MAP_KEY, user_agent="newGeocoder")
+
+
+# Function that formats the start/end date for the API request for historical weather
+def start_end(year):
+    today = datetime.now()
+    if len(str(today.month)) < 2:
+        month = "0" + str(today.month)
+    else:
+        month = str(today.month)
+    if len(str(today.day)) < 2:
+        day = "0" + str(today.day)
+    else:
+        day = str(today.day)
+    start_date = year + "-" + month + "-" + day
+    end_date = start_date
+    return start_date, end_date
+
+
+# Function used to locate "sunrise", "sunset", and "moon_phase" within the current weather
+def astro(weather, param):
+    sky = weather["forecast"]["forecastday"][0]["astro"][param]
+    return sky
+
+
+# Function that cleans a dataframe by splitting a column and renaming the columns it split
 # For the historical weather data, the splitter is "T"
 # For the current weather data, the splitter is " "
-# Function call will likely happen in a callback function.
-
-
 def weather_clean(df, splitter, column):
     df1 = df["Hour"].str.split(splitter, expand=True)
     df1[column] = df[column]
@@ -18,6 +48,7 @@ def weather_clean(df, splitter, column):
     return df1
 
 
+# Function that filters current weather API response, creates a df, and cleans it:
 def cw_filter(weather, col, weather_var):
     data = {
         "Hour": [x["time"] for x in weather["forecast"]["forecastday"][0]["hour"]],
@@ -28,7 +59,90 @@ def cw_filter(weather, col, weather_var):
     return clean_df
 
 
+# Function that filters the current weather and finds the high and low temp for the day:
 def high_low(weather):
     high = weather["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
     low = weather["forecast"]["forecastday"][0]["day"]["mintemp_f"]
     return high, low
+
+
+# Function that creates the ddk.DataCards and ddk.Card to hold weather information:
+def weather_card(date, rise, set, moon, high, low, fig):
+    sky_block = ddk.Card(
+        id=date,
+        children=[
+            ddk.DataCard(
+                id=date + "_sunrise", value=rise, label="Sunrise", icon="sun", width=100
+            ),
+            ddk.DataCard(
+                id=date + "_sunset",
+                value=set,
+                label="Sunset",
+                icon="cloud-sun",
+                width=100,
+            ),
+            ddk.DataCard(
+                id=date + "_moon_phase",
+                value=moon,
+                label="Moon Phase",
+                icon="moon",
+                width=100,
+            ),
+            ddk.DataCard(
+                id=date + "_high_temp",
+                value=high,
+                label="High Temperature",
+                icon="temperature-high",
+                width=100,
+            ),
+            ddk.DataCard(
+                id=date + "_low_temp",
+                value=low,
+                label="Low Temperature",
+                icon="temperature-low",
+                width=100,
+            ),
+        ],
+    )
+    rain_fig = ddk.Card(
+        children=[ddk.CardHeader(title="Hourly Chance of Rain"), ddk.Graph(figure=fig)]
+    )
+
+    return sky_block, rain_fig
+
+
+# This function incorporates all of the above functions to geocode the address, make API request, filter data, clean it,
+# make vizualizations, and create cards for the app layout
+# This function will be called within 2 callbacks.
+def get_weather(address, year):
+    # Geocoding the address
+    location = locator.geocode(address)
+    # Grabbing the latitude and longitude
+    lat = location.latitude
+    lon = location.longitude
+    # Making the api request for the current weather.
+    current_weather = requests.get(
+        f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={lat},{lon}&days=7&aqi=no&alerts=no"
+    ).json()
+    # Getting today's date to be used in the ids of the ddk.DataCards
+    today = current_weather["location"]["localtime"][:10]
+    # Using the astro func defined in api_utils.py to get the sunrise, sunset, and moon phase
+    sunrise = astro(current_weather, "sunrise")
+    sunset = astro(current_weather, "sunset")
+    moon_phase = astro(current_weather, "moon_phase")
+    high_temp, low_temp = high_low(current_weather)
+
+    # filtering the current weather to create a df of the chance_of_rain
+
+    precip = cw_filter(current_weather, "Rain Chance", "chance_of_rain")
+    # Creating a bar graph for the Rain Chance:
+    rain_today = px.bar(precip, x="Hour", y="Rain Chance")
+
+    # Establishing the start and end date for the historical weather api request
+    start_date, end_date = start_end(year)
+
+    # Using the weather_card() to create the cards for the dash app:
+    weather_today = weather_card(
+        today, sunrise, sunset, moon_phase, high_temp, low_temp, rain_today
+    )
+    return weather_today
